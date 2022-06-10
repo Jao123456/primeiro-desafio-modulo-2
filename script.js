a0bfc77a7cad6fe858e0f3965068707ba8ae834d
joaoguilherme.ifrjcsg.20201@gmail.com <joaoguilherme.souza.ifrjcsg.20201@gmail.com>
2022-06-09 23:40:31 -0300

    desafio de logica

---
+++ script.js
@@ -0,0 +1,19 @@
+let nome = "João";
+console.log(nome);
+
+let idade = 18
+console.log(idade)
+
+let ano = 2022;
+console.log(ano - idade);
+
+let soma = 2004;
+console.log(idade + ano);
+
+let text = "Eu estou mexendo no console.log";
+console.log(text);
+
+let quartaFeira = false;
+console.log(typeof quartaFeira);
+console.log (quartaFeira);
+
